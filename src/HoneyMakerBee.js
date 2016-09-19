b6eac35995a5112128cefdace74e84a41b40f4ef
Nick Englund <englundn@gmail.com>
2016-09-19 12:34:24 -0700

    Added honeyMakerBee functionality.

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,7 +1,20 @@
 import Bee from './Bee'
 
-class HoneyMakerBee {
-  // TODO..
+class HoneyMakerBee extends Bee {
+  constructor () {
+    super();
+    this.age = 10;
+    this.job = 'make honey';
+    this.honeyPot = 0;
+  }
+};
+
+HoneyMakerBee.prototype.makeHoney = function() {
+  this.honeyPot++;
+};
+
+HoneyMakerBee.prototype.giveHoney = function() {
+  this.honeyPot--;
 };
 
 export default HoneyMakerBee;
